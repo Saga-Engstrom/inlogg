88a9b2193d08b9e5dcc9a19605ac1f8b525a421f
Saga Engström <Saga97sle@hotmail.com>
2026-02-12 14:49:17 +0100

    Added 3rd flexbox

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
 const upperBox = document.getElementById("upperBox");
+const middleBox = document.getElementById("middleBox");
 const lowerBox = document.getElementById("lowerBox");
-const outputBox = document.getElementById("outputBox");
 
 //show login box and instructions
 function showLogin() {
   const loginlabel = document.createElement("label");
   loginlabel.innerText = "Skriv in ditt användarnamn och lösenord";
+
   const loginBtn = document.createElement("button");
+  loginBtn.innerText = "Logga in";
+
   let usernameElmnt = document.createElement("input");
   usernameElmnt.setAttribute("placeholder", "Användarnamn");
+
   let passwordElmnt = document.createElement("input");
   passwordElmnt.setAttribute("placeholder", "Lösenord");
   passwordElmnt.setAttribute("type", "password");
-  loginBtn.innerText = "Login";
+
+  const registerBtn = document.createElement("button");
+  registerBtn.innerText = "Registera dig";
 
   upperBox.appendChild(loginlabel);
-  lowerBox.appendChild(usernameElmnt);
-  lowerBox.appendChild(passwordElmnt);
-  lowerBox.appendChild(loginBtn);
+  middleBox.appendChild(usernameElmnt);
+  middleBox.appendChild(passwordElmnt);
+  middleBox.appendChild(loginBtn);
+  lowerBox.appendChild(registerBtn);
 
   loginBtn.addEventListener("click", function () {
     verify(usernameElmnt.value, passwordElmnt.value);
@@ -27,27 +34,27 @@ function showLogin() {
 function verify(username, password) {
   if (username == "test" && password == "1234") {
     upperBox.innerHTML = "";
+    middleBox.innerHTML = "";
     lowerBox.innerHTML = "";
-    outputBox.innerHTML = "";
     console.log("Rätt input");
     showLogedin(username);
   }
 
   upperBox.innerHTML = "";
+  middleBox.innerHTML = "";
   lowerBox.innerHTML = "";
-  outputBox.innerHTML = "";
   showLogin();
 
   if (username != "test") {
     const wrongName = document.createElement("text");
     wrongName.innerText = "Fel användarnamn";
-    outputBox.appendChild(wrongName);
+    lowerBox.appendChild(wrongName);
     console.log("Fel användarnamn");
   }
   if (password != "1234") {
     const wrongPassword = document.createElement("text");
     wrongPassword.innerText = "Fel lösenord";
-    outputBox.appendChild(wrongPassword);
+    lowerBox.appendChild(wrongPassword);
     console.log("Fel lösenord");
   }
 }
@@ -60,7 +67,7 @@ function showLogedin(username) {
   welcomeText.innerText = "Välkommen " + username;
 
   upperBox.appendChild(logoutBtn);
-  lowerBox.appendChild(welcomeText);
+  middleBox.appendChild(welcomeText);
   console.log("Login");
   logoutBtn.addEventListener("click", function () {
     logingOut();
@@ -70,8 +77,8 @@ function showLogedin(username) {
 //logout switch to login page
 function logingOut() {
   upperBox.innerHTML = "";
+  middleBox.innerHTML = "";
   lowerBox.innerHTML = "";
-  outputBox.innerHTML = "";
 
   showLogin();
 }
